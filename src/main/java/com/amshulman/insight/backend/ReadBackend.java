4de34554176a6fbbfc93bf7a68735e85c82e7b9e
Andrew Shulman <andy.shulman@hotmail.com>
2014-08-19 17:30:41 -0400

    Add method to get worlds from database

--- src/main/java/com/amshulman/insight/backend/ReadBackend.java
+++ src/main/java/com/amshulman/insight/backend/ReadBackend.java
@@ -1,5 +1,7 @@
 package com.amshulman.insight.backend;
 
+import java.util.Set;
+
 import com.amshulman.insight.query.QueryParameterBuilder;
 import com.amshulman.insight.query.QueryParameters;
 import com.amshulman.insight.results.InsightResultSet;
@@ -7,6 +9,8 @@ import com.amshulman.insight.results.InsightResultSet;
 public interface ReadBackend extends AutoCloseable {
 
     public abstract InsightResultSet submit(QueryParameters params);
+
+    public Set<String> getWorlds();
 
     public QueryParameterBuilder newQueryBuilder();
 
