6d3aa55774c68940fdc77c09f8e6d622cce63974
Andrew Shulman <andy.shulman@hotmail.com>
2014-09-07 15:53:36 -0400

    Add UUID->name lookup method

---
+++ src/main/java/com/amshulman/insight/util/PlayerUtil.java
@@ -0,0 +1,44 @@
+package com.amshulman.insight.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+import com.google.common.base.CharMatcher;
+
+public class PlayerUtil {
+
+    private static final String USER_AGENT = "Insight";
+    private static final String SESSION_SERVER = "https://sessionserver.mojang.com/session/minecraft/profile/";
+
+    public static String getCurrentName(UUID uuid) {
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL(SESSION_SERVER + uuid.toString().replace("-", "")).openConnection();
+            con.setRequestMethod("GET");
+
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            String name = response.toString().replaceAll("\"properties\":\\[.*?\\],", "").replaceAll(".+?\"name\":\"([A-Za-z0-9_]+)\".*", "$1");
+
+            if (name.isEmpty() || !CharMatcher.JAVA_LETTER_OR_DIGIT.matchesAllOf(name)) {
+                return uuid.toString().replace("-", "");
+            }
+
+            return name;
+        } catch (IOException e) {
+            return uuid.toString().replace("-", "");
+        }
+    }
+}
