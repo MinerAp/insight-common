3521317d7c8c53e8ad0e23d5e70a90a240ae707b
Andrew Shulman <andy.shulman@hotmail.com>
2016-09-10 20:44:03 -0400

    Update skull serialization to enable rollbacks
    Also remove now-unused PlayerHeads dependency.

--- src/main/java/com/amshulman/insight/serialization/SkullMeta.java
+++ src/main/java/com/amshulman/insight/serialization/SkullMeta.java
@@ -2,44 +2,34 @@ package com.amshulman.insight.serialization;
 
 import lombok.Value;
 
+import java.util.UUID;
+
 import org.bukkit.Material;
 import org.bukkit.SkullType;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Skull;
 import org.bukkit.inventory.ItemStack;
 
-import com.amshulman.insight.playerheads.PlayerHeadsBridge;
-
 @Value
 public final class SkullMeta implements MetadataEntry {
 
     private static final long serialVersionUID = -3025864226932077542L;
 
-    transient static boolean playerHeads;
-
     BlockFace rotation;
     SkullType type;
-    String owner;
-    String displayName;
-
-    static {
-        playerHeads = playerHeadsLoaded();
-    }
+    UUID uuid;
+    String name;
 
     public SkullMeta(Skull skull) {
         rotation = skull.getRotation();
         type = skull.getSkullType();
 
         if (skull.hasOwner()) {
-            owner = skull.getOwningPlayer().getName();
-            if (playerHeads) {
-                displayName = PlayerHeadsBridge.getDisplayName(owner);
-            } else {
-                displayName = null;
-            }
+            uuid = skull.getOwningPlayer().getUniqueId();
+            name = skull.getOwningPlayer().getName();
         } else {
-            owner = null;
-            displayName = null;
+            uuid = null;
+            name = null;
         }
     }
 
@@ -47,22 +37,10 @@ public final class SkullMeta implements MetadataEntry {
         ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) type.ordinal());
         if (SkullType.PLAYER.equals(type)) {
             org.bukkit.inventory.meta.SkullMeta skullMeta = (org.bukkit.inventory.meta.SkullMeta) skull.getItemMeta();
-            skullMeta.setOwner(owner);
-            if (displayName != null) {
-                skullMeta.setDisplayName(displayName);
-            }
+            skullMeta.setOwner(name);
             skull.setItemMeta(skullMeta);
         }
 
         return skull;
-    }
-
-    private static boolean playerHeadsLoaded() {
-        try {
-            Class.forName(PlayerHeadsBridge.class.getName());
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
     }
 }
